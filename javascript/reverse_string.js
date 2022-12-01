337cd5e0499472551ed1276f6c8f5772ebc5ec94
+ <+>
2022-12-01 13:34:10 -0500

    commit tree 1fcfd75a88f0349a8a7cf55aa78d133787aec09d

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,6 +1,15 @@
 function reverseString(str) {
-  // type your code here
+
+  for (let i = 0; i < str.length; i++) {
+    let newArray= []
+    newArray.push(i)
+    newArray.join('')
+    console.log(newArray)
+  }
+
+  return newArray
 }
+
 
 if (require.main === module) {
   // add your own tests in here
@@ -17,3 +26,10 @@ module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// We're going to iterate through this array first with the MAP method, but we could also use a for loop
+// While iterating, we are creating a new constant that it's gonna be a empty array. This array will be the recipient for our reversed string
+//For each item, we are add the item to the new array with the push method
+//Afterwards, we will join the elements of the array into a string with the method array.join(''). 
+//An empty parameter will join the elements of the array without spaces
+
